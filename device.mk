809e6db47e13543431962ffd237a02ea7b935faa
Pranav Vashi <neobuddy89@gmail.com>
2020-04-19 16:40:36 +0530

    angler: Remove IMS wrappers
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Marc Babad <blitzkrieg.marc02@gmail.com>

--- device.mk
+++ device.mk
@@ -297,18 +297,6 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS := \
     device/huawei/angler/overlay
 
-PRODUCT_PACKAGES += \
-    telephony-ext \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
    fs_config_files
