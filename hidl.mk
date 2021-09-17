8741a689f0e29c78f47d1fb0185387189043541a
Marc Babad <blitzkrieg.marc02@gmail.com>
2021-09-18 00:15:38 +0800

    angler: Add clearkey 1.2 DRM support
    Change-Id: I0bf8bff5eb80fd1e8740b41c751e57019b98d2d1

--- hidl.mk
+++ hidl.mk
@@ -44,7 +44,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 # Fingerprint Sensor
 PRODUCT_PACKAGES += \
